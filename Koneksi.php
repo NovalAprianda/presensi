36c46998ad8330105aba6283649073067a52c190
NovalAprianda <18101817110017@mhs.ulm.ac.id>
2020-12-16 12:22:27 +0800

    koneksi db cloud

--- Koneksi.php
+++ Koneksi.php
@@ -1,6 +1,22 @@
-<?php 
+<?php
+
+//Koneksi Di PC 
+// $host = 'localhost';
+// $db = 'ccpresensi';
+// $user =  'root';
+// $pass = '';
+// $charset = 'utf8mb4';
+// $dsn = "mysql:host=$host;dbname=$db;charset=$charset"; 
+
+//Koneksi di Cloud
+$host = 'remotemysql.com';
+$db = 'uk13PWwp0W';
+$user =  'uk13PWwp0W';
+$pass = 'y9Xnu1JRtp';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset"; 
 try { 
-$koneksi = new PDO('mysql:host=localhost;dbname=ccpresensi', 'root', '',
+$koneksi = new PDO($dsn, $user, $pass,
 array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION,PDO::ATTR_PERSISTENT => true)); 
 } 
 catch(PDOException $e) { 
